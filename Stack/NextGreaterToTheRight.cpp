000154988c84bd4f171e51e6cc61271f27948f1f
saura <saurabh0497@gmail.com>
2022-06-07 00:24:17 +0530

    Stack Changes Committed

---
+++ Stack/NextGreaterToTheRight.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> nextGreaterToTheRight(vector<int>vec){
+    vector<int> v;
+    stack<int>stack;
+
+    for(int i=vec.size()-1;i>=0;i--){
+        if(stack.empty()){
+            v.push_back(-1);
+            stack.push(vec[i]);
+        }
+        else if(vec[i]<stack.top()){
+            v.push_back(stack.top());
+            stack.push(vec[i]);
+        }
+        else if(vec[i]>=stack.top()){
+            while(!stack.empty() && vec[i]>=stack.top()){
+                stack.pop();
+            }
+            if(stack.empty()){
+                 v.push_back(-1);
+                stack.push(vec[i]);
+            }
+            if(stack.top()>vec[i]){
+                v.push_back(stack.top());
+                stack.push(vec[i]);
+            }
+        }
+    }
+    reverse(v.begin(),v.end());
+    return v;
+}
+int main(){
+
+        vector<int> vec= {15,20};
+        vector<int>v=nextGreaterToTheRight(vec);
+        for(int i=0;i<v.size();i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+
+    return 0;
+}
